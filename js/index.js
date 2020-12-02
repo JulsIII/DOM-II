41bf750bf0554a6d913f5b73a8f9ea284420042f
Your Name <you@example.com>
2020-12-01 19:50:39 -0600

    3 - Editing sets

--- js/index.js
+++ js/index.js
@@ -3,7 +3,8 @@
 const mainNavHeader = document.querySelector(".main-navigation");
 const navContainer = document.querySelector(".nav-container");
 const logoHeading = document.querySelector(".logo-heading");
-const navLinks = document.querySelector(".nav");
+const navMain = document.querySelector('nav');
+const navLinks = document.querySelectorAll("a");
 
 const homeContainer = document.querySelector(".container home");
 const introHeader = document.querySelector(".intro");
@@ -13,26 +14,118 @@ const contentSecC = document.querySelector(".content-destination");
 const contentSecD = document.querySelector(".content-pick");
 
 const siteFooter = document.querySelector(".footer");
+const footerText = document.querySelectorAll("p");
 
+//Events
+logoHeading.addEventListener("mouseover", function (event) {
+    logoHeading.style.color = '#00ff00';
+    //console.log('ASDF');
+   });
+logoHeading.addEventListener("mouseout", function (event) {
+    logoHeading.style.color = '#0000ff';
+    //console.log('ASDF');
+   });
+
+
+navLinks[0].addEventListener("mouseover", function (event) {
+    navLinks[0].style.color = '#00ff00';
+    console.log('ASDF');
+   });
+navLinks[0].addEventListener("mouseout", function (event) {
+    navLinks[0].style.color = '#0000ff';
+    console.log('ASDF');
+   });
+navLinks[1].addEventListener("mouseover", function (event) {
+    navLinks[1].style.color = '#00ff00';
+    console.log('ASDF');
+   });
+navLinks[1].addEventListener("mouseout", function (event) {
+    navLinks[1].style.color = '#0000ff';
+    console.log('ASDF');
+   });
+navLinks[2].addEventListener("mouseover", function (event) {
+    navLinks[2].style.color = '#00ff00';
+    console.log('ASDF');
+   });
+navLinks[2].addEventListener("mouseout", function (event) {
+    navLinks[2].style.color = '#0000ff';
+    console.log('ASDF');
+   });
+navLinks[3].addEventListener("mouseover", function (event) {
+    navLinks[3].style.color = '#00ff00';
+    console.log('ASDF');
+   });
+navLinks[3].addEventListener("mouseout", function (event) {
+    navLinks[3].style.color = '#0000ff';
+    console.log('ASDF');
+   });
 introHeader.addEventListener("mouseover", function (event) {
     introHeader.style.color = '#00ff00';
     console.log('ASDF');
    });
-
 introHeader.addEventListener("mouseout", function (event) {
     introHeader.style.color = '#0000ff';
     console.log('ASDF');
    });
 
 
-// const introImg = document.querySelector('.intro img')
-// introImg.addEventListener('mouseover', (event) => {
-//     introImg.style.opacity = '0.5'
-// })
-// // 7        mouseout - A pointing device is moved off the element that has the listener attached or off one of its children.
-// introImg.addEventListener('mouseout', (event) => {
-//     introImg.style.opacity = '1'
-// })
+introHeader.addEventListener("mouseover", function (event) {
+    introHeader.style.color = '#00ff00';
+    console.log('ASDF');
+   });
+introHeader.addEventListener("mouseout", function (event) {
+    introHeader.style.color = '#0000ff';
+    console.log('ASDF');
+   });
+
+
+contentSecA.addEventListener("mouseover", function (event) {
+    contentSecA.style.color = '#00ff00';
+    console.log('ASDF');
+   });
+contentSecA.addEventListener("mouseout", function (event) {
+    contentSecA.style.color = '#0000ff';
+    console.log('ASDF');
+   });
+
+contentSecB.addEventListener("mouseover", function (event) {
+    contentSecB.style.color = '#00ff00';
+    console.log('ASDF');
+   });
+contentSecB.addEventListener("mouseout", function (event) {
+    contentSecB.style.color = '#0000ff';
+    console.log('ASDF');
+   });
+
+contentSecC.addEventListener("mouseover", function (event) {
+    contentSecC.style.color = '#00ff00';
+    console.log('ASDF');
+   });
+contentSecC.addEventListener("mouseout", function (event) {
+    contentSecC.style.color = '#0000ff';
+    console.log('ASDF');
+   });
+
+contentSecD.addEventListener("mouseover", function (event) {
+    contentSecD.style.color = '#00ff00';
+    console.log('ASDF');
+   });
+contentSecD.addEventListener("mouseout", function (event) {
+    contentSecD.style.color = '#0000ff';
+    console.log('ASDF');
+   });
+
+footerText[9].addEventListener("mouseover", function (event) {
+    footerText[9].style.color = '#00ff00';
+    console.log('ASDFAAAAA');
+   });
+footerText[9].addEventListener("mouseout", function (event) {
+    footerText[9].style.color = '#0000ff';
+    console.log('ASDFAAAA');
+   });
+
+
+
 
 
 
